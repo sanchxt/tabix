fc161375566f85a46cef87d0d44cd979ac7f91ad
sanchxt <tpbeast007@gmail.com>
2024-12-07 18:45:30 +0530

    define 3xl screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/entrypoints/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1920px",
+      },
+    },
   },
   plugins: [],
 };
