26e80c31338cb3d90877558bf370308a50e35976
sanchxt <tpbeast007@gmail.com>
2024-12-24 18:51:09 +0530

    fix minor bugs, update naming

--- src/entrypoints/newtab/components/BottomBar.tsx
+++ src/entrypoints/newtab/components/BottomBar.tsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 
 import BouncingArrow from "./BouncingArrow";
 import { Game } from "../utils/types/bottom-bar";
-import { GameA, GameB, GameC, GameD } from "./games";
+import { PomodoroTimer, GameB, GameC, GameD } from "./games";
 
 import "@/assets/newtab/bottom-bar.css";
 
-const games: Game[] = ["gameA", "gameB", "gameC", "gameD"];
+const games: Game[] = ["Pomodoro", "gameB", "gameC", "gameD"];
 
 const BottomBar = () => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
-  const [selectedGame, setSelectedGame] = useState<Game>("gameA");
+  const [selectedGame, setSelectedGame] = useState<Game>("Pomodoro");
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
@@ -22,8 +22,8 @@ const BottomBar = () => {
 
   const renderGame = () => {
     switch (selectedGame) {
-      case "gameA":
-        return <GameA />;
+      case "Pomodoro":
+        return <PomodoroTimer />;
       case "gameB":
         return <GameB />;
       case "gameC":
@@ -42,29 +42,33 @@ const BottomBar = () => {
       {/* bottom bar */}
       <div
         className={`absolute ${
-          isExpanded ? "bottom-0" : "-bottom-40 lg:-bottom-36"
-        } transition-all duration-500 w-full rounded-t-2xl md:rounded-t-[2rem] min-h-48 border-2 border-b-0 border-white bg-black/50`}
-        onClick={toggleExpand}
+          isExpanded ? "bottom-1" : "-bottom-40 lg:-bottom-36"
+        } transition-all duration-500 w-full rounded-t-2xl md:rounded-t-[2rem] h-48 border-2 border-b-0 border-white bg-slate-950/[98%]`}
         onMouseEnter={() => setIsExpanded(true)}
         onMouseLeave={() => setIsExpanded(false)}
       >
-        <div className="text-white text-center">
-          <p className="text-base lg:text-xl font-bold">{renderGame()}</p>
+        <div className="text-white text-center h-full w-full rounded-t-[2rem]">
+          <p className="text-base lg:text-xl font-bold h-full">
+            {renderGame()}
+          </p>
         </div>
 
-        <div className="grid grid-rows-4 h-full bg-white">
-          {games.map((game, index) => (
-            <button
-              key={index}
-              className={`game-box ${
-                selectedGame === game ? "border-2 border-blue-500" : ""
-              }`}
-              onClick={() => handleGameSelect(game)}
-            >
-              {`Game ${index + 1}`}
-            </button>
-          ))}
-        </div>
+        {/* game buttons - visible only when expanded */}
+        {isExpanded && (
+          <div className="grid grid-rows-4 h-full">
+            {games.map((game, index) => (
+              <button
+                key={index}
+                className={`game-box ${
+                  selectedGame === game ? "border-2 border-blue-500" : ""
+                }`}
+                onClick={() => handleGameSelect(game)}
+              >
+                {`Tool ${index + 1}`}
+              </button>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
